0c569544025d09ef53aaff85abbb0c1c2f1061e4
CarmJos <KarmunJ@outlook.com>
2022-03-13 18:49:23 +0800

    [1.0.0] 初始版本完成

--- src/main/java/cc/carm/plugin/scriptitems/configuration/PluginMessages.java
+++ src/main/java/cc/carm/plugin/scriptitems/configuration/PluginMessages.java
@@ -7,7 +7,7 @@ import cc.carm.lib.easyplugin.configuration.language.MessagesRoot;
 public class PluginMessages extends MessagesRoot {
 
     public static final EasyMessageList USAGE = EasyMessageList.builder().contents(
-            "&2&l脚本指令 &f指令帮助",
+            "&2&l脚本物品 &f指令帮助",
             "&8#&f give &a<玩家名> &a<脚本ID> &2[数量]",
             "&8-&7 给予指定玩家指定数量的物品。",
             "&8#&f apply &a<脚本ID>",
